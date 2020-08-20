4253329a55ec09e34dc9149aacdefb61603f9fee
Nirav Bavishi <niravbavishi007@gmail.com>
2020-08-20 13:58:03 -0400

    Initial Commit

---
+++ RealDecoyChallengeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealDecoyChallengeApp
+//
+//  Created by Nirav Bavishi on 2020-08-20.
+//  Copyright © 2020 Nirav Bavishi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
